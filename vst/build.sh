2b22d6914d3423240c6c558deaae86f49f4257fe
Tom Havlik <keres993@gmail.com>
2020-04-25 12:04:45 -0500

    ...

--- vst/build.sh
+++ vst/build.sh
@@ -2,14 +2,42 @@
 set -euo pipefail
 cd $(dirname $0)
 target=release
-cargo build --$target
+
+if grep -qEi "(Microsoft|WSL)" /proc/version &>/dev/null; then
+  WSL_DETECTED=1
+  echo "WSL detected"
+else
+  WSL_DETECTED=0
+fi
+
+if [[ $WSL_DETECTED == "1" ]]; then
+  set +e
+  win_userprofile="$(cmd.exe /c "<nul set /p=%UserProfile%" 2>/dev/null)"
+  set -e
+  win_userprofile=$(wslpath -u "$win_userprofile")
+  cargo_bin="$win_userprofile/.cargo/bin/cargo.exe"
+  echo "Using cargo.exe at $cargo_bin"
+  $cargo_bin build --$target
+else
+  cargo build --$target
+fi
+
+outdir=../target/$target/plugins
+mkdir -p $outdir
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  ./package.sh paradise ../target/$target/libparadise.dylib
-  outdir=../target/release/plugins
+  echo "Packaging MacOS plugin..."
+  ./scripts/package_macos.sh paradise ../target/$target/libparadise.dylib
   mkdir -p $outdir || true
   rm -rf $outdir/paradise.vst || true
   mv paradise.vst $outdir
+elif [[ $WSL_DETECTED == "1" ]]; then
+  echo "Packaging Windows plugin..."
+  rm $outdir/paradise.dll || true
+  cp ../target/$target/paradise.dll $outdir
+else
+  echo "Unknown platform"
+  exit 1
 fi
 
 #mkdir -p ../target/$target/paradise.vst/Contents/MacOS

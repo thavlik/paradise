3b63de7cd90e502e6d9ca4aefd2d0a370aae0285
Tom Havlik <thomashavlik@Adams-Mac-mini.local>
2020-04-25 10:41:35 -0500

    ...

--- vst/src/lib.rs
+++ vst/src/lib.rs
@@ -33,18 +33,14 @@ struct RemoteAudioEffect {
     // Send streams
     tx: Vec<std::sync::Arc<stream::tx::TxStream<stream::tx::locking::LockingTxBuffer>>>,
 
+    // Shared runtime (for threading)
     rt: std::sync::Arc<tokio::runtime::Runtime>,
 
     // Store a handle to the plugin's parameter object.
-    params: Arc<LadderParameters>,
-    // the output of the different filter stages
-    vout: [f32; 4],
-    // s is the "state" parameter. In an IIR it would be the last value from the filter
-    // In this we find it by trapezoidal integration to avoid the unit delay
-    s: [f32; 4],
+    params: Arc<RemoteAudioEffectParameters>,
 }
 
-struct LadderParameters {
+struct RemoteAudioEffectParameters {
     // the "cutoff" parameter. Determines how heavy filtering is
     cutoff: AtomicFloat,
     g: AtomicFloat,
@@ -77,10 +73,10 @@ fn entrypoint() {
     info!("Start successfully");
 }
 
-impl Default for LadderParameters {
-    fn default() -> LadderParameters {
+impl Default for RemoteAudioEffectParameters {
+    fn default() -> RemoteAudioEffectParameters {
         START.call_once(|| entrypoint());
-        LadderParameters {
+        RemoteAudioEffectParameters {
             cutoff: AtomicFloat::new(1000.),
             res: AtomicFloat::new(2.),
             poles: AtomicUsize::new(3),
@@ -91,78 +87,14 @@ impl Default for LadderParameters {
         }
     }
 }
-// member methods for the struct
+
 impl RemoteAudioEffect {
     fn running(&mut self) -> bool {
         false
     }
-    // the state needs to be updated after each process. Found by trapezoidal integration
-    fn update_state(&mut self) {
-        self.s[0] = 2. * self.vout[0] - self.s[0];
-        self.s[1] = 2. * self.vout[1] - self.s[1];
-        self.s[2] = 2. * self.vout[2] - self.s[2];
-        self.s[3] = 2. * self.vout[3] - self.s[3];
-    }
-    // performs a complete filter process (mystran's method)
-    fn tick_pivotal(&mut self, input: f32) {
-        if self.params.drive.get() > 0. {
-            self.run_ladder_nonlinear(input * (self.params.drive.get() + 0.7));
-        } else {
-            //
-            self.run_ladder_linear(input);
-        }
-        self.update_state();
-    }
-    // nonlinear ladder filter function with distortion.
-    fn run_ladder_nonlinear(&mut self, input: f32) {
-        let mut a = [1f32; 5];
-        let base = [input, self.s[0], self.s[1], self.s[2], self.s[3]];
-        // a[n] is the fixed-pivot approximation for tanh()
-        for n in 0..base.len() {
-            if base[n] != 0. {
-                a[n] = base[n].tanh() / base[n];
-            } else {
-                a[n] = 1.;
-            }
-        }
-        // denominators of solutions of individual stages. Simplifies the math a bit
-        let g0 = 1. / (1. + self.params.g.get() * a[1]);
-        let g1 = 1. / (1. + self.params.g.get() * a[2]);
-        let g2 = 1. / (1. + self.params.g.get() * a[3]);
-        let g3 = 1. / (1. + self.params.g.get() * a[4]);
-        //  these are just factored out of the feedback solution. Makes the math way easier to read
-        let f3 = self.params.g.get() * a[3] * g3;
-        let f2 = self.params.g.get() * a[2] * g2 * f3;
-        let f1 = self.params.g.get() * a[1] * g1 * f2;
-        let f0 = self.params.g.get() * g0 * f1;
-        // outputs a 24db filter
-        self.vout[3] =
-            (f0 * input * a[0] + f1 * g0 * self.s[0] + f2 * g1 * self.s[1] + f3 * g2 * self.s[2] + g3 * self.s[3])
-                / (f0 * self.params.res.get() * a[3] + 1.);
-        // since we know the feedback, we can solve the remaining outputs:
-        self.vout[0] = g0
-            * (self.params.g.get() * a[1] * (input * a[0] - self.params.res.get() * a[3] * self.vout[3]) + self.s[0]);
-        self.vout[1] = g1 * (self.params.g.get() * a[2] * self.vout[0] + self.s[1]);
-        self.vout[2] = g2 * (self.params.g.get() * a[3] * self.vout[1] + self.s[2]);
-    }
-    // linear version without distortion
-    pub fn run_ladder_linear(&mut self, input: f32) {
-        // denominators of solutions of individual stages. Simplifies the math a bit
-        let g0 = 1. / (1. + self.params.g.get());
-        let g1 = self.params.g.get() * g0 * g0;
-        let g2 = self.params.g.get() * g1 * g0;
-        let g3 = self.params.g.get() * g2 * g0;
-        // outputs a 24db filter
-        self.vout[3] =
-            (g3 * self.params.g.get() * input + g0 * self.s[3] + g1 * self.s[2] + g2 * self.s[1] + g3 * self.s[0])
-                / (g3 * self.params.g.get() * self.params.res.get() + 1.);
-        // since we know the feedback, we can solve the remaining outputs:
-        self.vout[0] = g0 * (self.params.g.get() * (input - self.params.res.get() * self.vout[3]) + self.s[0]);
-        self.vout[1] = g0 * (self.params.g.get() * self.vout[0] + self.s[1]);
-        self.vout[2] = g0 * (self.params.g.get() * self.vout[1] + self.s[2]);
-    }
 }
-impl LadderParameters {
+
+impl RemoteAudioEffectParameters {
     pub fn set_cutoff(&self, value: f32) {
         // cutoff formula gives us a natural feeling cutoff knob that spends more time in the low frequencies
         self.cutoff.set(20000. * (1.8f32.powf(10. * value - 10.)));
@@ -178,7 +110,7 @@ impl LadderParameters {
         self.poles.store(((value * 3.).round()) as usize, Ordering::Relaxed);
     }
 }
-impl PluginParameters for LadderParameters {
+impl PluginParameters for RemoteAudioEffectParameters {
     // get_parameter has to return the value used in set_parameter
     fn get_parameter(&self, index: i32) -> f32 {
         match index {
@@ -236,9 +168,7 @@ impl Default for RemoteAudioEffect {
             .build()
             .unwrap());
         RemoteAudioEffect {
-            vout: [0f32; 4],
-            s: [0f32; 4],
-            params: Arc::new(LadderParameters::default()),
+            params: Arc::new(RemoteAudioEffectParameters::default()),
             rx: vec![],
             tx: vec![],
             rt,

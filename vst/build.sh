894f37963974a3e3535c17ad528939b5c6998463
Tom Havlik <keres993@gmail.com>
2020-04-25 12:05:12 -0500

    ...

--- vst/build.sh
+++ vst/build.sh
@@ -40,13 +40,3 @@ else
   exit 1
 fi
 
-#mkdir -p ../target/$target/paradise.vst/Contents/MacOS
-#echo '<?xml version="1.0" encoding="UTF-8"?>
-#<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-#<plist version="1.0">
-#<dict>
-#    <key>CFBundleName</key>
-#    <string>Paradise</string>
-#</dict>
-#</plist>' > ../target/release/paradise.vst/Contents/Info.plist
-#cp ../target/release/libparadise.dylib ../target/release/paradise.vst/Contents/MacOS/paradise

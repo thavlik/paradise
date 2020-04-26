448dfac6c0da136ece5dac8e38eae3789b25f075
Tom Havlik <keres993@gmail.com>
2020-04-26 13:04:41 -0500

    ...

--- vst3/scripts/build.sh
+++ vst3/scripts/build.sh
@@ -10,6 +10,11 @@ else
   WSL_DETECTED=0
 fi
 
+extra_args=""
+if [ $target == "release" ]; then
+  extra_args="--$target"
+fi
+
 if [[ $WSL_DETECTED == "1" ]]; then
   set +e
   win_userprofile="$(cmd.exe /c "<nul set /p=%UserProfile%" 2>/dev/null)"
@@ -17,9 +22,10 @@ if [[ $WSL_DETECTED == "1" ]]; then
   win_userprofile=$(wslpath -u "$win_userprofile")
   cargo_bin="$win_userprofile/.cargo/bin/cargo.exe"
   echo "Using cargo.exe at $cargo_bin"
-  $cargo_bin build --$target
+
+  $cargo_bin build $extra_args
 else
-  cargo build --$target
+  cargo build $extra_args
 fi
 
 outdir=../../target/$target/plugins
@@ -27,17 +33,17 @@ outdir=../../target/$target/plugins
 mkdir -p $outdir
 vst_search_dir=$outdir
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  echo "Packaging MacOS VST2 plugin..."
-  ./package_macos.sh paradise ../target/$target/libparadise.dylib
+  echo "Packaging MacOS VST3 plugin..."
+  ./osx-bundler.sh paradise ../target/$target/libparadise.dylib
   mkdir -p $outdir || true
   rm -rf $outdir/paradise.vst || true
-  mv paradise.vst $outdir
-  echo "Successfully built VST2 plugin for MacOS"
+  mv paradise.vst3 $outdir
+  echo "Successfully VST3 built plugin for MacOS"
 elif [[ $WSL_DETECTED == "1" ]]; then
-  echo "Packaging Windows VST2 plugin..."
-  rm $outdir/paradise.dll || true
-  cp ../../target/$target/paradise.dll $outdir
-  echo "Successfully built VST2 plugin for Windows"
+  echo "Packaging Windows VST3 plugin..."
+  rm $outdir/paradise.vst3 || true
+  cp ../../target/$target/paradise_vst3.dll $outdir/paradise.vst3
+  echo "Successfully built VST3 plugin for Windows"
   vst_search_dir=$(wslpath -w $vst_search_dir)
 else
   echo "Unknown platform"

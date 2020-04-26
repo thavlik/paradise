d3a220bb8c1824403af7de68a87aec793e026bab
Tom Havlik <keres993@gmail.com>
2020-04-26 14:59:53 -0500

    ...

--- daemon/src/cmd/mod.rs
+++ daemon/src/cmd/mod.rs
@@ -1,1 +1,19 @@
+use clap::Clap;
+
 pub mod info;
+
+#[derive(Clap)]
+pub enum SubCommand {
+    /// Enumerate IO details on all audio devices
+    #[clap(name = "info")]
+    Info(info::InfoArgs),
+}
+
+/// Bare metal daemon for Paradise audio engine
+#[derive(Clap)]
+#[clap(version = "1.0", author = "Tom Havlik")]
+pub struct Opts {
+    #[clap(subcommand)]
+    pub subcmd: SubCommand,
+}
+

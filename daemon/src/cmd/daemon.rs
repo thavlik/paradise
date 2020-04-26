4cd450108d8da7b8f3ec4608d7e8da7dd2b900af
Tom Havlik <keres993@gmail.com>
2020-04-26 16:13:34 -0500

    ...

--- daemon/src/cmd/daemon.rs
+++ daemon/src/cmd/daemon.rs
@@ -11,7 +11,6 @@ pub struct DaemonArgs {
 
 pub fn main(args: DaemonArgs) -> Result<(), anyhow::Error> {
     let host = cpal::default_host();
-
     // Default devices.
     let input_device = host
         .default_input_device()
@@ -21,36 +20,17 @@ pub fn main(args: DaemonArgs) -> Result<(), anyhow::Error> {
         .expect("failed to get default output device");
     println!("Using default input device: \"{}\"", input_device.name()?);
     println!("Using default output device: \"{}\"", output_device.name()?);
-
-    // We'll try and use the same configuration between streams to keep it simple.
     let config: cpal::StreamConfig = input_device.default_input_config()?.into();
-
     let input_data_fn = move |data: &[f32]| {
-
+        println!("input len: {}", data.len());
     };
-
     let output_data_fn = move |data: &mut [f32]| {
-
+        println!("output len: {}", data.len());
     };
-
-    // Build streams.
-    println!(
-        "Attempting to build both streams with f32 samples and `{:?}`.",
-        config
-    );
     let input_stream = input_device.build_input_stream(&config, input_data_fn, err_fn)?;
     let output_stream = output_device.build_output_stream(&config, output_data_fn, err_fn)?;
-    println!("Successfully built streams.");
-
-    // Play the streams.
-    println!(
-        "Starting the input and output streams with `{}` milliseconds of latency.",
-        LATENCY_MS
-    );
     input_stream.play()?;
     output_stream.play()?;
-
-    // Run for 3 seconds before closing.
     println!("Playing for 3 seconds... ");
     std::thread::sleep(std::time::Duration::from_secs(3));
     drop(input_stream);

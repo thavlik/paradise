3513a59bb72af6d50691c702da02d0a0ab3e2ef9
Tom Havlik <keres993@gmail.com>
2020-04-26 15:36:48 -0500

    ...

--- daemon/src/cmd/daemon.rs
+++ daemon/src/cmd/daemon.rs
@@ -2,7 +2,7 @@
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use ringbuf::RingBuffer;
 
-const LATENCY_MS: f32 = 150.0;
+const LATENCY_MS: f32 = 0.0 ;//150.0;
 
 /// A subcommand for controlling testing
 #[derive(clap::Clap)]
@@ -25,50 +25,12 @@ pub fn main(args: DaemonArgs) -> Result<(), anyhow::Error> {
     // We'll try and use the same configuration between streams to keep it simple.
     let config: cpal::StreamConfig = input_device.default_input_config()?.into();
 
-    // Create a delay in case the input and output devices aren't synced.
-    let latency_frames = (LATENCY_MS / 1_000.0) * config.sample_rate.0 as f32;
-    let latency_samples = latency_frames as usize * config.channels as usize;
-
-    // The buffer to share samples
-    let ring = RingBuffer::new(latency_samples * 2);
-    let (mut producer, mut consumer) = ring.split();
-
-    // Fill the samples with 0.0 equal to the length of the delay.
-    for _ in 0..latency_samples {
-        // The ring buffer has twice as much space as necessary to add latency here,
-        // so this should never fail
-        producer.push(0.0).unwrap();
-    }
-
     let input_data_fn = move |data: &[f32]| {
-        let mut output_fell_behind = false;
-        for &sample in data {
-            if producer.push(sample).is_err() {
-                output_fell_behind = true;
-            }
-        }
-        if output_fell_behind {
-            eprintln!("output stream fell behind: try increasing latency");
-        }
+
     };
 
     let output_data_fn = move |data: &mut [f32]| {
-        let mut input_fell_behind = None;
-        for sample in data {
-            *sample = match consumer.pop() {
-                Ok(s) => s,
-                Err(err) => {
-                    input_fell_behind = Some(err);
-                    0.0
-                }
-            };
-        }
-        if let Some(err) = input_fell_behind {
-            eprintln!(
-                "input stream fell behind: {:?}: try increasing latency",
-                err
-            );
-        }
+
     };
 
     // Build streams.

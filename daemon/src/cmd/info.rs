f567d90f3fa5883a2f12395e9416a29af04d8edf
Tom Havlik <thomashavlik@Adams-Mac-mini.local>
2020-04-26 16:11:09 -0500

    ...

--- daemon/src/cmd/info.rs
+++ daemon/src/cmd/info.rs
@@ -32,7 +32,7 @@ pub fn main(args: InfoArgs) -> Result<(), anyhow::Error> {
                     println!("  {}. \"{}\"", device_index, name);
                 },
                 Err(e) => {
-                    println!("Device {}: {:?}", device_index, e);
+                    println!("Device {} error: {:?}", device_index, e);
                     continue;
                 },
             }

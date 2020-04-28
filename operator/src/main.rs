0f2a7cf6b14ec79b64474e78e60876c9c76f3a32
Tom Havlik <keres993@gmail.com>
2020-04-27 20:54:49 -0500

    ...

--- operator/src/main.rs
+++ operator/src/main.rs
@@ -21,20 +21,6 @@ use node::{
 
 
 fn main() {
-
-
-    let inputs = (0..128).map(|i| Arc::new(InterfaceIO::new(
-        i,
-        false,
-        None,
-    ))).collect();
-    let outputs = (0..128).map(|i| Arc::new(InterfaceIO::new(
-        i,
-        true,
-        None,
-    ))).collect();
-    let iface = Interface::new(inputs, outputs);
-
     //let result = astar(&Pos(1, 1), |p| p.successors(), |_| 0,
     //                   |p| *p == GOAL);
     //assert_eq!(result.expect("no path found").1, 4);

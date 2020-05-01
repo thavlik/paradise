ee4ff4f92bdc4dd6de0428bc94a0a3c9a1cd6ce5
Tom Havlik <thomashavlik@Adams-Mac-mini.local>
2020-04-30 21:57:37 -0500

    ...

---
+++ device/include/paradise.h
@@ -0,0 +1,10 @@
+#include <cstdarg>
+#include <cstdint>
+#include <cstdlib>
+#include <new>
+
+extern "C" {
+
+void rust_function();
+
+} // extern "C"
---
+++ device/src/lib.rs
@@ -0,0 +1,12 @@
+#[no_mangle]
+pub extern "C" fn rust_function() {
+
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}

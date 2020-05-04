bd38a26007821403e5dae141d0e610ee9759afc5
Tom Havlik <thomashavlik@Adams-Mac-mini.local>
2020-05-03 23:25:39 -0500

    ...

--- cli/src/cmd/patch.rs
+++ cli/src/cmd/patch.rs
@@ -9,6 +9,8 @@ use std::{
 use anyhow::{anyhow, bail, Error, Context, Result};
 use futures::{StreamExt, TryFutureExt};
 use structopt::{self, StructOpt};
+use tracing::{error, info, info_span};
+use tracing_futures::Instrument as _;
 
 /// A subcommand for controlling testing
 #[derive(clap::Clap)]
@@ -187,6 +189,7 @@ pub async fn main(args: PatchArgs) -> Result<()> {
             server_config.certificate(quinn::CertificateChain::from_certs(vec![cert]), key)?;
         }
 
+        /*
         let mut endpoint = quinn::Endpoint::builder();
         endpoint.listen(server_config.build());
 
@@ -209,6 +212,7 @@ pub async fn main(args: PatchArgs) -> Result<()> {
                 }),
             );
         }
+        */
 
     } else {
         // TODO: Record audio, send it to addr
@@ -247,4 +251,113 @@ pub async fn main(args: PatchArgs) -> Result<()> {
 
 fn err_fn(err: cpal::StreamError) {
     eprintln!("an error occurred on stream: {}", err);
+}
+
+
+async fn handle_connection(root: Arc<Path>, conn: quinn::Connecting) -> Result<()> {
+    let quinn::NewConnection {
+        connection,
+        mut bi_streams,
+        ..
+    } = conn.await?;
+    let span = info_span!(
+        "connection",
+        remote = %connection.remote_address(),
+        protocol = %connection
+            .authentication_data()
+            .protocol
+            .map_or_else(|| "<none>".into(), |x| String::from_utf8_lossy(&x).into_owned())
+    );
+    async {
+        info!("established");
+
+        // Each stream initiated by the client constitutes a new request.
+        while let Some(stream) = bi_streams.next().await {
+            let stream = match stream {
+                Err(quinn::ConnectionError::ApplicationClosed { .. }) => {
+                    info!("connection closed");
+                    return Ok(());
+                }
+                Err(e) => {
+                    return Err(e);
+                }
+                Ok(s) => s,
+            };
+            tokio::spawn(
+                handle_request(root.clone(), stream)
+                    .unwrap_or_else(move |e| error!("failed: {reason}", reason = e.to_string()))
+                    .instrument(info_span!("request")),
+            );
+        }
+        Ok(())
+    }
+        .instrument(span)
+        .await?;
+    Ok(())
+}
+
+async fn handle_request(
+    root: Arc<Path>,
+    (mut send, recv): (quinn::SendStream, quinn::RecvStream),
+) -> Result<()> {
+    let req = recv
+        .read_to_end(64 * 1024)
+        .await
+        .map_err(|e| anyhow!("failed reading request: {}", e))?;
+    let mut escaped = String::new();
+    for &x in &req[..] {
+        let part = ascii::escape_default(x).collect::<Vec<_>>();
+        escaped.push_str(str::from_utf8(&part).unwrap());
+    }
+    info!(content = %escaped);
+    // Execute the request
+    let resp = process_get(&root, &req).unwrap_or_else(|e| {
+        error!("failed: {}", e);
+        format!("failed to process request: {}\n", e)
+            .into_bytes()
+            .into()
+    });
+    // Write the response
+    send.write_all(&resp)
+        .await
+        .map_err(|e| anyhow!("failed to send response: {}", e))?;
+    // Gracefully terminate the stream
+    send.finish()
+        .await
+        .map_err(|e| anyhow!("failed to shutdown stream: {}", e))?;
+    info!("complete");
+    Ok(())
+}
+
+fn process_get(root: &Path, x: &[u8]) -> Result<Box<[u8]>> {
+    if x.len() < 4 || &x[0..4] != b"GET " {
+        bail!("missing GET");
+    }
+    if x[4..].len() < 2 || &x[x.len() - 2..] != b"\r\n" {
+        bail!("missing \\r\\n");
+    }
+    let x = &x[4..x.len() - 2];
+    let end = x.iter().position(|&c| c == b' ').unwrap_or_else(|| x.len());
+    let path = str::from_utf8(&x[..end]).context("path is malformed UTF-8")?;
+    let path = Path::new(&path);
+    let mut real_path = PathBuf::from(root);
+    let mut components = path.components();
+    match components.next() {
+        Some(path::Component::RootDir) => {}
+        _ => {
+            bail!("path must be absolute");
+        }
+    }
+    for c in components {
+        match c {
+            path::Component::Normal(x) => {
+                real_path.push(x);
+            }
+            x => {
+                bail!("illegal component in path: {:?}", x);
+            }
+        }
+    }
+    let data = fs::read(&real_path).context("failed reading file")?;
+    Ok(data.into())
 }

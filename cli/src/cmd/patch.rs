2778cf441c123ad823fdc74600aef8768a9d917b
Tom Havlik <thomashavlik@Adams-Mac-mini.local>
2020-05-03 23:17:04 -0500

    ...

--- cli/src/cmd/patch.rs
+++ cli/src/cmd/patch.rs
@@ -1,5 +1,14 @@
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-use anyhow::{Error, Result};
+use std::{
+    ascii, fs, io,
+    net::SocketAddr,
+    path::{self, Path, PathBuf},
+    str,
+    sync::Arc,
+};
+use anyhow::{anyhow, bail, Error, Context, Result};
+use futures::{StreamExt, TryFutureExt};
+use structopt::{self, StructOpt};
 
 /// A subcommand for controlling testing
 #[derive(clap::Clap)]
@@ -35,6 +44,14 @@ pub struct PatchArgs {
     /// Sample rate. Default allows the device to choose.
     #[clap(long = "sample-rate")]
     sample_rate: Option<usize>,
+
+    ///
+    #[clap(parse(from_os_str), long = "key")]
+    key: Option<PathBuf>,
+
+    ///
+    #[clap(parse(from_os_str), long = "key")]
+    cert: Option<PathBuf>,
 }
 
 type TxStream = paradise_core::stream::tx::udp::UdpTxStream<
@@ -43,6 +60,9 @@ type TxStream = paradise_core::stream::tx::udp::UdpTxStream<
 type RxStream = paradise_core::stream::rx::udp::UdpRxStream<
     paradise_core::stream::rx::locking::LockingRxBuffer,
 >;
+
+#[allow(unused)]
+pub const ALPN_QUIC_HTTP: &[&[u8]] = &[b"hq-27"];
 
 pub async fn main(args: PatchArgs) -> Result<()> {
     let host = match &args.host {
@@ -57,24 +77,22 @@ pub async fn main(args: PatchArgs) -> Result<()> {
             host
         },
     };
+
     let (addr, device_is_output) = match (&args.source, &args.sink) {
         (Some(source), Some(sink)) => return Err(Error::msg("source and sink cannot be specified at the same time")),
         (None, None) => return Err(Error::msg("you must specify a source or sink address")),
-        (Some(source), _) => {
-            // Source address, sink audio output
-            (source, true)
-        },
-        (_, Some(sink)) => {
-            // Source audio input, sink address
-            (sink, false)
-        },
+        (Some(source), _) => (source, true), // Source address, sink device output
+        (_, Some(sink)) => (sink, false), // Source device input, sink address
     };
+
+    let addr: SocketAddr = addr.parse()?;
+
     let device: cpal::Device = match args.device {
         Some(name) => {
             match name.parse::<usize>() {
                 Ok(index) => {
                     if index >= host.devices()?.count() {
-                        return Err(anyhow::Error::msg(format!("device index out of range (tip: run info)")));
+                        return Err(Error::msg(format!("device index out of range (tip: run info)")));
                     }
                     match host.devices()?
                         .skip(index)
@@ -83,7 +101,7 @@ pub async fn main(args: PatchArgs) -> Result<()> {
                             println!("found device {}. \"{}\"", &name, device.name().unwrap_or(String::from("NULL")));
                             device
                         },
-                        None => return Err(anyhow::Error::msg(format!("device at index \"{}\" not found (tip: run info)", &name))),
+                        None => return Err(Error::msg(format!("device at index \"{}\" not found (tip: run info)", &name))),
                     }
                 },
                 _ => match host.devices()?
@@ -96,7 +114,7 @@ pub async fn main(args: PatchArgs) -> Result<()> {
                         println!("found device \"{}\"", &name);
                         d
                     },
-                    None => return Err(anyhow::Error::msg(format!("device \"{}\" not found", name))),
+                    None => return Err(Error::msg(format!("device \"{}\" not found", name))),
                 },
             }
         },
@@ -106,7 +124,7 @@ pub async fn main(args: PatchArgs) -> Result<()> {
                     println!("using default output device \"{}\"", &device.name().unwrap_or(String::from("NULL")));
                     device
                 },
-                None => return Err(anyhow::Error::msg(format!("default output device not available"))),
+                None => return Err(Error::msg(format!("default output device not available"))),
             }
         } else {
             match host.default_input_device() {
@@ -114,11 +132,64 @@ pub async fn main(args: PatchArgs) -> Result<()> {
                     println!("using default input device \"{}\"", &device.name().unwrap_or(String::from("NULL")));
                     device
                 },
-                None => return Err(anyhow::Error::msg(format!("default input device not available"))),
+                None => return Err(Error::msg(format!("default input device not available"))),
             }
         },
     };
 
+    if device_is_output {
+        // TODO: Listen on addr, play on device
+        let mut transport_config = quinn::TransportConfig::default();
+        transport_config.stream_window_uni(0);
+        let mut server_config = quinn::ServerConfig::default();
+        server_config.transport = std::sync::Arc::new(transport_config);
+        let mut server_config = quinn::ServerConfigBuilder::new(server_config);
+        server_config.protocols(ALPN_QUIC_HTTP);
+        //server_config.use_stateless_retry(true);
+
+        if let (Some(key_path), Some(cert_path)) = (&args.key, &args.cert) {
+            let key = fs::read(key_path).context("failed to read private key")?;
+            let key = if key_path.extension().map_or(false, |x| x == "der") {
+                quinn::PrivateKey::from_der(&key)?
+            } else {
+                quinn::PrivateKey::from_pem(&key)?
+            };
+            let cert_chain = fs::read(cert_path).context("failed to read certificate chain")?;
+            let cert_chain = if cert_path.extension().map_or(false, |x| x == "der") {
+                quinn::CertificateChain::from_certs(quinn::Certificate::from_der(&cert_chain))
+            } else {
+                quinn::CertificateChain::from_pem(&cert_chain)?
+            };
+            server_config.certificate(cert_chain, key)?;
+        } else {
+            let dirs = directories::ProjectDirs::from("org", "quinn", "quinn-examples").unwrap();
+            let path = dirs.data_local_dir();
+            let cert_path = path.join("cert.der");
+            let key_path = path.join("key.der");
+            let (cert, key) = match fs::read(&cert_path).and_then(|x| Ok((x, fs::read(&key_path)?))) {
+                Ok(x) => x,
+                Err(ref e) if e.kind() == io::ErrorKind::NotFound => {
+                    println!("generating self-signed certificate");
+                    let cert = rcgen::generate_simple_self_signed(vec!["localhost".into()]).unwrap();
+                    let key = cert.serialize_private_key_der();
+                    let cert = cert.serialize_der().unwrap();
+                    fs::create_dir_all(&path).context("failed to create certificate directory")?;
+                    fs::write(&cert_path, &cert).context("failed to write certificate")?;
+                    fs::write(&key_path, &key).context("failed to write private key")?;
+                    (cert, key)
+                }
+                Err(e) => {
+                    panic!("failed to read certificate: {}", e);
+                }
+            };
+            let key = quinn::PrivateKey::from_der(&key)?;
+            let cert = quinn::Certificate::from_der(&cert)?;
+            server_config.certificate(quinn::CertificateChain::from_certs(vec![cert]), key)?;
+        }
+
+    } else {
+        // TODO: Record audio, send it to addr
+    }
 
     /*
 

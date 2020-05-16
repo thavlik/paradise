6a019540b775170ece48c83e7064d70c46cfdba7
Tom Havlik <keres993@gmail.com>
2020-05-16 16:46:22 -0500

    ...

--- cli/src/cmd/device/create.rs
+++ cli/src/cmd/device/create.rs
@@ -31,44 +31,52 @@ use tracing_futures::Instrument as _;
 #[allow(unused)]
 pub const ALPN_QUIC_HTTP: &[&[u8]] = &[b"hq-27"];
 
-#[tokio::test(threaded_scheduler)]
-async fn socket() {
-    let mut transport_config = TransportConfig::default();
-    transport_config.stream_window_uni(0);
-    let mut server_config = ServerConfig::default();
-    server_config.transport = std::sync::Arc::new(transport_config);
-    let mut server_config = ServerConfigBuilder::new(server_config);
-    server_config.protocols(ALPN_QUIC_HTTP);
-    let dirs = directories::ProjectDirs::from("org", "quinn", "quinn-examples").unwrap();
-    let path = dirs.data_local_dir();
-    let cert_path = path.join("cert.der");
-    let key_path = path.join("key.der");
-    let (cert, key) = match fs::read(&cert_path).and_then(|x| Ok((x, fs::read(&key_path).unwrap()))) {
-        Ok(x) => x,
-        Err(ref e) if e.kind() == io::ErrorKind::NotFound => {
-            let cert = rcgen::generate_simple_self_signed(vec!["localhost".into()]).unwrap();
-            let key = cert.serialize_private_key_der();
-            let cert = cert.serialize_der().unwrap();
-            fs::create_dir_all(&path).context("failed to create certificate directory").unwrap();
-            fs::write(&cert_path, &cert).context("failed to write certificate").unwrap();
-            fs::write(&key_path, &key).context("failed to write private key").unwrap();
-            (cert, key)
-        }
-        Err(e) => {
-            panic!("failed to read certificate: {}", e);
-        }
-    };
-    let key = PrivateKey::from_der(&key).unwrap();
-    let cert = Certificate::from_der(&cert).unwrap();
-    server_config.certificate(CertificateChain::from_certs(vec![cert]), key).unwrap();
-    let port = portpicker::pick_unused_port().expect("pick port");
-    let addr: SocketAddr = format!("127.0.0.1:{}", port).parse().unwrap();
-    let mut endpoint = Endpoint::builder();
-    endpoint.listen(server_config.build());
-    let mut incoming = {
-        let (endpoint, incoming) = endpoint.bind(&addr).unwrap();
-        incoming
-    };
+#[test]
+fn socket() {
+    tokio::runtime::Builder::new()
+        .threaded_scheduler()
+        .build()
+        .unwrap()
+        .block_on(async move {
+            let mut transport_config = TransportConfig::default();
+            transport_config.stream_window_uni(0);
+            let mut server_config = ServerConfig::default();
+            server_config.transport = std::sync::Arc::new(transport_config);
+            let mut server_config = ServerConfigBuilder::new(server_config);
+            server_config.protocols(ALPN_QUIC_HTTP);
+            let dirs = directories::ProjectDirs::from("org", "quinn", "quinn-examples").unwrap();
+            let path = dirs.data_local_dir();
+            let cert_path = path.join("cert.der");
+            let key_path = path.join("key.der");
+            let (cert, key) = match fs::read(&cert_path).and_then(|x| Ok((x, fs::read(&key_path).unwrap()))) {
+                Ok(x) => x,
+                Err(ref e) if e.kind() == io::ErrorKind::NotFound => {
+                    let cert = rcgen::generate_simple_self_signed(vec!["localhost".into()]).unwrap();
+                    let key = cert.serialize_private_key_der();
+                    let cert = cert.serialize_der().unwrap();
+                    fs::create_dir_all(&path).context("failed to create certificate directory").unwrap();
+                    fs::write(&cert_path, &cert).context("failed to write certificate").unwrap();
+                    fs::write(&key_path, &key).context("failed to write private key").unwrap();
+                    (cert, key)
+                }
+                Err(e) => {
+                    panic!("failed to read certificate: {}", e);
+                }
+            };
+            let key = PrivateKey::from_der(&key).unwrap();
+            let cert = Certificate::from_der(&cert).unwrap();
+            server_config.certificate(CertificateChain::from_certs(vec![cert]), key).unwrap();
+            let port = portpicker::pick_unused_port().expect("pick port");
+            let addr: SocketAddr = format!("127.0.0.1:{}", port).parse().unwrap();
+            let mut endpoint = Endpoint::builder();
+            endpoint.listen(server_config.build());
+            /*
+            let mut incoming = {
+    let (endpoint, incoming) = endpoint.bind(&addr).unwrap();
+    incoming
+};
+ */
+        });
 }
 
 #[derive(Serialize, Deserialize)]

6306f47a4e2f578202bea0ae8713bebbbdf6e5d1
Tom Havlik <thomashavlik@Adams-Mac-mini.local>
2020-05-16 15:46:46 -0500

    ...

--- device/platform/build.sh
+++ device/platform/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# This script is intended to be called from `cargo make` within cli/
+# Generally, you will not invoke it manually.
 set -euo pipefail
 cd $(dirname $0)
 if [ $CARGO_MAKE_RUST_TARGET_OS == "macos" ]; then

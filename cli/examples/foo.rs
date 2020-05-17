f81a513799e4ebfe7c8701e3b74f1f0c69185ac4
Tom Havlik <thomashavlik@Adams-Mac-mini.local>
2020-05-16 20:01:39 -0500

    ...

---
+++ cli/examples/foo.rs
@@ -0,0 +1,81 @@
+use anyhow::{Error, Result};
+use cpal::traits::{DeviceTrait, HostTrait};
+use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
+use std::process::Command;
+use uuid::Uuid;
+use quinn::{
+    ServerConfig,
+    ServerConfigBuilder,
+    Endpoint,
+    TransportConfig,
+    CertificateChain,
+    PrivateKey,
+    Certificate,
+};
+use std::{
+    io,
+    net::SocketAddr,
+    sync::{Arc, mpsc},
+    fs,
+};
+use futures::{
+    future::FutureExt,
+    pin_mut,
+    select,
+};
+use anyhow::Context;
+use tracing::{error, info, info_span};
+use tracing_futures::Instrument as _;
+
+#[allow(unused)]
+pub const ALPN_QUIC_HTTP: &[&[u8]] = &[b"hq-27"];
+
+async fn socket() {
+    // No panic - can find runtime
+    tokio::spawn(async move {});
+
+    let mut transport_config = TransportConfig::default();
+    transport_config.stream_window_uni(0);
+    let mut server_config = ServerConfig::default();
+    server_config.transport = Arc::new(transport_config);
+    let mut server_config = ServerConfigBuilder::new(server_config);
+    server_config.protocols(ALPN_QUIC_HTTP);
+    let dirs = directories::ProjectDirs::from("org", "quinn", "quinn-examples").unwrap();
+    let path = dirs.data_local_dir();
+    let cert_path = path.join("cert.der");
+    let key_path = path.join("key.der");
+    let (cert, key) = match fs::read(&cert_path).and_then(|x| Ok((x, fs::read(&key_path).unwrap()))) {
+        Ok(x) => x,
+        Err(ref e) if e.kind() == io::ErrorKind::NotFound => {
+            let cert = rcgen::generate_simple_self_signed(vec!["localhost".into()]).unwrap();
+            let key = cert.serialize_private_key_der();
+            let cert = cert.serialize_der().unwrap();
+            fs::create_dir_all(&path).context("failed to create certificate directory").unwrap();
+            fs::write(&cert_path, &cert).context("failed to write certificate").unwrap();
+            fs::write(&key_path, &key).context("failed to write private key").unwrap();
+            (cert, key)
+        }
+        Err(e) => {
+            panic!("failed to read certificate: {}", e);
+        }
+    };
+    let key = PrivateKey::from_der(&key).unwrap();
+    let cert = Certificate::from_der(&cert).unwrap();
+    server_config.certificate(CertificateChain::from_certs(vec![cert]), key).unwrap();
+    let port = portpicker::pick_unused_port().expect("pick port");
+    let addr: SocketAddr = format!("127.0.0.1:{}", port).parse().unwrap();
+    let mut endpoint = Endpoint::builder();
+    endpoint.listen(server_config.build());
+    let mut incoming = {
+        let (endpoint, incoming) = endpoint.bind(&addr).unwrap(); // PANIC - cannot find runtime
+        incoming
+    };
+    println!("All good!");
+}
+
+
+#[tokio::main]
+async fn main() {
+    socket().await;
+}
--- cli/src/cmd/device/create.rs
+++ cli/src/cmd/device/create.rs
@@ -16,14 +16,10 @@ use quinn::{
 use std::{
     io,
     net::SocketAddr,
-    sync::mpsc,
+    sync::{Arc, mpsc},
     fs,
 };
-use futures::{
-    future::FutureExt,
-    pin_mut,
-    select,
-};
+
 use anyhow::Context;
 use tracing::{error, info, info_span};
 use tracing_futures::Instrument as _;
@@ -33,12 +29,10 @@ pub const ALPN_QUIC_HTTP: &[&[u8]] = &[b"hq-27"];
 
 #[tokio::test(threaded_scheduler)]
 async fn socket() {
-    // No panic - can find runtime
-    tokio::spawn(async move {});
     let mut transport_config = TransportConfig::default();
     transport_config.stream_window_uni(0);
     let mut server_config = ServerConfig::default();
-    server_config.transport = std::sync::Arc::new(transport_config);
+    server_config.transport = Arc::new(transport_config);
     let mut server_config = ServerConfigBuilder::new(server_config);
     server_config.protocols(ALPN_QUIC_HTTP);
     let dirs = directories::ProjectDirs::from("org", "quinn", "quinn-examples").unwrap();
@@ -67,10 +61,10 @@ async fn socket() {
     let addr: SocketAddr = format!("127.0.0.1:{}", port).parse().unwrap();
     let mut endpoint = Endpoint::builder();
     endpoint.listen(server_config.build());
-    //let mut incoming = {
-    //    let (endpoint, incoming) = endpoint.bind(&addr).unwrap(); // PANIC - cannot find runtime
-    //    incoming
-    //};
+    let mut incoming = {
+        let (endpoint, incoming) = endpoint.bind(&addr).unwrap();
+        incoming
+    };
 }
 
 #[derive(Serialize, Deserialize)]
@@ -155,6 +149,7 @@ mod macos {
     use std::os::unix::fs::PermissionsExt;
     use std::sync::{Arc, Mutex};
     use std::time::{Duration, SystemTime};
+    use futures::{StreamExt, TryFutureExt};
 
     lazy_static! {
         static ref CORE_AUDIO_LOCK: Mutex<()> = Mutex::new(());
@@ -376,19 +371,58 @@ ManufacturerName = "{}";
             device.verify().expect_err("should not exist");
         }
 
+        async fn handle_connection(conn: quinn::Connecting) -> Result<()> {
+            Ok(())
+        }
+
         #[tokio::test(threaded_scheduler)]
         async fn basic_stream() {
             let (mut send_stop, recv_stop) = mpsc::channel::<()>();
             tokio::spawn(async move {
-                recv_stop.recv();
-                //while let Some(conn) = incoming.next().await {
-                //    info!("connection incoming");
-                //    tokio::spawn(
-                //        handle_connection(root.clone(), conn).unwrap_or_else(move |e| {
-                //            error!("connection failed: {reason}", reason = e.to_string())
-                //        }),
-                //    );
-                //}
+                let mut transport_config = TransportConfig::default();
+                transport_config.stream_window_uni(0);
+                let mut server_config = ServerConfig::default();
+                server_config.transport = Arc::new(transport_config);
+                let mut server_config = ServerConfigBuilder::new(server_config);
+                server_config.protocols(ALPN_QUIC_HTTP);
+                let dirs = directories::ProjectDirs::from("org", "quinn", "quinn-examples").unwrap();
+                let path = dirs.data_local_dir();
+                let cert_path = path.join("cert.der");
+                let key_path = path.join("key.der");
+                let (cert, key) = match fs::read(&cert_path).and_then(|x| Ok((x, fs::read(&key_path).unwrap()))) {
+                    Ok(x) => x,
+                    Err(ref e) if e.kind() == io::ErrorKind::NotFound => {
+                        let cert = rcgen::generate_simple_self_signed(vec!["localhost".into()]).unwrap();
+                        let key = cert.serialize_private_key_der();
+                        let cert = cert.serialize_der().unwrap();
+                        fs::create_dir_all(&path).context("failed to create certificate directory").unwrap();
+                        fs::write(&cert_path, &cert).context("failed to write certificate").unwrap();
+                        fs::write(&key_path, &key).context("failed to write private key").unwrap();
+                        (cert, key)
+                    }
+                    Err(e) => {
+                        panic!("failed to read certificate: {}", e);
+                    }
+                };
+                let key = PrivateKey::from_der(&key).unwrap();
+                let cert = Certificate::from_der(&cert).unwrap();
+                server_config.certificate(CertificateChain::from_certs(vec![cert]), key).unwrap();
+                let port = portpicker::pick_unused_port().expect("pick port");
+                let addr: SocketAddr = format!("127.0.0.1:{}", port).parse().unwrap();
+                let mut endpoint = Endpoint::builder();
+                endpoint.listen(server_config.build());
+                let mut incoming = {
+                    let (endpoint, incoming) = endpoint.bind(&addr).unwrap();
+                    incoming
+                };
+                while let Some(conn) = incoming.next().await {
+                    info!("connection incoming");
+                    tokio::spawn(
+                        handle_connection(conn).unwrap_or_else(move |e| {
+                            error!("connection failed: {reason}", reason = e.to_string())
+                        }),
+                    );
+                }
             });
             let _l = CORE_AUDIO_LOCK.lock().unwrap();
             cleanup();

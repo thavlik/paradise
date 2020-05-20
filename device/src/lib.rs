16dfe7b8e8f470f0679433364c4f4fc0365818c5
Tom Havlik <thomashavlik@Adams-Mac-mini.local>
2020-05-20 01:23:30 -0500

    ...

--- device/src/lib.rs
+++ device/src/lib.rs
@@ -6,7 +6,8 @@ extern crate log;
 extern crate lazy_static;
 #[macro_use]
 extern crate futures;
-
+#[macro_use]
+extern crate serde;
 use crossbeam::channel::{Sender, Receiver};
 use std::{ptr, ffi::{c_void, CStr}};
 use std::path::PathBuf;
@@ -37,36 +38,6 @@ impl rustls::ServerCertVerifier for SkipServerVerification {
     ) -> Result<rustls::ServerCertVerified, rustls::TLSError> {
         Ok(rustls::ServerCertVerified::assertion())
     }
-}
-
-
-async fn run_client(server_addr: SocketAddr) -> Result<()> {
-    warn!("configuring client");
-    let client_cfg = configure_client();
-
-    warn!("building endpoint...");
-    let mut endpoint_builder = quinn::Endpoint::builder();
-    endpoint_builder.default_client_config(client_cfg);
-
-    let addr = "127.0.0.1:0".parse()?;
-    warn!("binding endpoint {}", &addr);
-    let (endpoint, _) = endpoint_builder.bind(&addr)?;
-
-    warn!("connecting to server...");
-    let quinn::NewConnection { connection, .. } = endpoint
-        .connect(&server_addr, "localhost")?
-        .await?;
-
-    warn!("[client] connected: addr={}", connection.remote_address());
-
-    // TODO: move connection ref into struct
-
-    // Dropping handles allows the corresponding objects to automatically shut down
-    drop(connection);
-    // Make sure the server has a chance to clean up
-    endpoint.wait_idle().await;
-
-    Ok(())
 }
 
 async fn connect(server_addr: SocketAddr) -> Result<(quinn::Endpoint, quinn::Connection)> {
@@ -143,51 +114,17 @@ lazy_static! {
         .unwrap()));
 }
 
-async fn endpoint_entry(server_addr: SocketAddr, stop: Receiver<()>) -> Result<()> {
-    run_client(server_addr).await;
-    Err(Error::msg("exited prematurely"))
-}
-
-async fn listen_for_stop(stop: Receiver<()>) -> Result<()> {
-    stop.recv().unwrap();
-    Err(Error::msg("exited prematurely"))
-}
-
 async fn driver_entry(driver: Arc<Driver>, stop: Receiver<()>) -> Result<()> {
     if driver.spec.endpoints.len() == 0 {
         return Err(Error::msg("no endpoints"));
     }
-
-    // TODO: reconnect logic
-    //let mut f = vec![];
-    //let mut stops = vec![];
     for endpoint in &driver.spec.endpoints {
         let driver = driver.clone();
         let endpoint = endpoint.clone();
         tokio::spawn(async move {
-            driver.connect_with_retry(endpoint);
+            driver.connect_with_retry(endpoint).await;
         });
-        //connect_with_retry(server_addr);
-
-        //f.push(connect(server_addr));
-        //f.push(endpoint_entry(server_addr, r));
-        //stops.push(s);
     }
-
-    //let mut results = futures::future::join_all(f).await;
-
-    //tokio::spawn(async move {
-    //    stop.recv().unwrap();
-    //    stops.into_iter()
-    //        .for_each(|s| {
-    //            s.send(()).unwrap()
-    //        });
-    //});
-
-    //tokio::spawn(async move {
-    //    futures::future::try_join_all(f).await;
-    //});
-
     Ok(())
 }
 
@@ -204,9 +141,15 @@ pub struct Driver {
     stop: Mutex<Sender<()>>,
 }
 
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+struct Frame {
+    buffer: Vec<u8>,
+    sample_time: f64,
+}
+
 impl Driver {
     /// "Fire and forget" connect method
-    fn connect_with_retry(&self, endpoint: Endpoint) {
+    async fn connect_with_retry(&self, endpoint: Endpoint) {
         let server_addr: SocketAddr = match endpoint.addr.parse() {
             Ok(v) => v,
             Err(e) => {
@@ -215,11 +158,7 @@ impl Driver {
             }
         };
         loop {
-            let (s, r) = crossbeam::channel::unbounded();
-            tokio::spawn(async move {
-                s.send(connect(server_addr.clone()).await);
-            });
-            match r.recv().unwrap() {
+            match connect(server_addr.clone()).await {
                 Ok((e, conn)) => {
                     match self.add_output(Output {
                         spec: endpoint,
@@ -232,7 +171,7 @@ impl Driver {
                         _ => {},
                     }
                     return;
-                }
+                },
                 Err(e) => {
                     error!("error connecting to {}: {}", &server_addr, e);
                     std::thread::sleep(std::time::Duration::from_secs(5));
@@ -251,14 +190,16 @@ impl Driver {
     }
 
     fn io_proc(&self, buffer: &[u8], sample_time: f64) -> Result<()> {
-        // TODO: package up buffer and sample_time into message
+        let payload = bytes::Bytes::from(bincode::serialize(&Frame{
+            buffer: Vec::from(buffer),
+            sample_time,
+        })?);
         let outputs = self.outputs.lock().unwrap();
         for output in &*outputs {
-            match output.conn.send_datagram(bytes::Bytes::new()) {
+            match output.conn.send_datagram(payload.clone()) {
                 Ok(()) => {}
                 Err(e) => {
-                    error!("failed to send datagram to '{}': {}", &output.spec.name, e);
-                    // TODO: decide if reconnect is appropriate here
+                    error!("failed to send datagram to output '{}': {}", &output.spec.name, e);
                 }
             }
         }
